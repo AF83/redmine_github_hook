7f80993fae00329a5fec2a198a70be36ce698f78
Alban Peignier <alban@tryphon.eu>
2015-11-30 11:54:47 +0100

    Use payload Hash when available

--- app/controllers/github_hook_controller.rb
+++ app/controllers/github_hook_controller.rb
@@ -23,6 +23,7 @@ class GithubHookController < ApplicationController
   private
 
   def parse_payload
+    return params[:payload] if Hash === params[:payload]
     JSON.parse(params[:payload] || "{}")
   end
 
